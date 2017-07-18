6da8fa82c7b8f15a720fccf33f525617b07e621b
Ivan Chub <ivansergchub@gmail.com>
2017-07-17 22:41:45 -0700

    landing page is slightly presentable

--- src/components/LandingPageComponent.tsx
+++ src/components/LandingPageComponent.tsx
@@ -1,49 +1,60 @@
 import * as React from "react";
 import * as Radium from "radium";
-import {INoteInfo, makeINoteInfo} from "../models/INoteInfo";
-import {NoteButtonComponent} from "./NoteButtonComponent";
 
 @Radium
 export class LandingPageComponent extends React.Component<any, ILandingPageComponentState> {
-    public state: ILandingPageComponentState;
-
-    constructor(props) {
-        super();
-
-        this.state = {
-            notes: [
-                makeINoteInfo("C", "/res/notes/C-Normal.mp3", "/res/notes/C-Shitty.mp3", "a"),
-                makeINoteInfo("D", "/res/notes/D-Normal.mp3", "/res/notes/D-Shitty.mp3", "s"),
-                makeINoteInfo("E", "/res/notes/E-Normal.mp3", "/res/notes/E-Shitty.mp3", "d"),
-                makeINoteInfo("F", "/res/notes/F-Normal.mp3", "/res/notes/F-Shitty.mp3", "f"),
-                makeINoteInfo("G", "/res/notes/G-Normal.mp3", "/res/notes/G-Shitty.mp3", "g"),
-                makeINoteInfo("A", "/res/notes/A-Normal.mp3", "/res/notes/A-Shitty.mp3", "h"),
-                makeINoteInfo("B", "/res/notes/B-Normal.mp3", "/res/notes/B-Shitty.mp3", "j")
-            ]
-        };
-    }
-
     public render() {
         return (
             <div>
                 <div style={[
-                    LandingPageComponent.styles.base
+                    LandingPageComponent.styles.base,
                 ]}>
-                    {
-                        this.state.notes.map(note => {
-                            return <NoteButtonComponent key={note.name} button={note}/>;
-                        })
-                    }
+                    <div style={[LandingPageComponent.styles.title]}>shitty recorder</div>
+                    <input
+                        style={[LandingPageComponent.styles.composeButton]}
+                        type="button"
+                        value="compose"
+                        onClick={this.onComposeClick.bind(this)}/>
                 </div>
             </div>
         );
     }
 
+    onComposeClick() {
+        window.location.href = "/composer";
+    }
+
     private static styles = {
-        base: {}
+        base: {
+            fontFamily: "'Oswald', sans-serif",
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+        },
+        title: {
+            fontSize: "4em",
+            color: "white",
+            backgroundColor: "black",
+        },
+        composeButton: {
+            padding: "10px 5px",
+            margin: "50px",
+            backgroundColor: "white",
+            color: "black",
+            border: "none",
+            fontFamily: "inherit",
+            textDecoration: "underline",
+            cursor: "pointer",
+            ":hover": {
+                color: "white",
+                backgroundColor: "black",
+            },
+        },
     };
 }
 
 export interface ILandingPageComponentState {
-    notes: INoteInfo[];
 }
