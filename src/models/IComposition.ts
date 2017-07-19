852601dae661152d3ee55005174836042d8a8451
Ivan Chub <ivansergchub@gmail.com>
2017-07-18 18:51:48 -0700

    made composition have a youtube video field

--- src/models/IComposition.ts
+++ src/models/IComposition.ts
@@ -8,17 +8,22 @@ export interface IComposition {
     compName: string
     compId: string
     notes: ICompositionNote[]
+    youtubeId: string;
 }
 
-export function makeIComposition(name: string, id: string, notes: ICompositionNote[]): IComposition {
+export function makeIComposition(name: string,
+                                 id: string,
+                                 notes: ICompositionNote[],
+                                 youtubeId: string): IComposition {
     return {
         compName: name,
         compId: id,
-        notes: notes
+        notes: notes,
+        youtubeId: youtubeId,
     };
 }
 
 export function makeNewIComposition(name: string, id: string): IComposition {
     let notes: ICompositionNote[] = [];
-    return makeIComposition(name, id, notes);
+    return makeIComposition(name, id, notes, "HQnC1UHBvWA");
 }
