1b520a5a123d3c47d0929e6856d30e056a00cecb
Ivan Chub <ivansergchub@gmail.com>
2017-07-18 18:49:16 -0700

    class for youtube api

---
+++ src/server/YoutubeApi.ts
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+export class YoutubeApi {
+    private constructor() {
+
+    }
+
+    public static getInfoOnVideo(videoId: string): Promise<IYoutubeVideoInfo> {
+        return axios.get(
+            `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=snippet&key=AIzaSyDXOagMe4Ozv3D3iFbtGxSiO4J82IcGXJo`)
+            .then((result) => {
+                return Promise.resolve(result.data);
+            });
+    }
+}
+
+export interface IYoutubeVideoInfo {
+    items: [{
+        snippet: {
+            title: string
+        }
+    }]
+}
