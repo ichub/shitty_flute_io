ec6719056383e804d3fdfb2b4852beccf95f29c3
briangu33 <briangu33@gmail.com>
2017-07-20 02:40:27 -0700

    delete old files

--- src/server/ComposerTokenLoader.ts
+++ src/server/ComposerTokenLoader.ts
@@ -5,7 +5,7 @@
 export const tokenLength = 20;
 
 export function generateToken(): string {
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let ret = "";
     for (let i = 0; i < tokenLength; i++) {
         ret += chars[Math.floor(Math.random() * chars.length)];
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import {InitializationState} from "../models/IInitializationState";
 import {LandingPageComponent} from "../components/pages/LandingPageComponent";
-import {ComposerPageComponent} from "../components/pages/ComposerPageComponent";
 import {RecorderPlayerPageComponent} from "../components/pages/RecorderPlayerPageComponent";
 import {StyleRoot} from "radium";
 
@@ -14,16 +13,6 @@ if (initializedState.pageName === "landing") {
             <LandingPageComponent/>
         </StyleRoot>,
         document.getElementById("app-container"));
-} else if (initializedState.pageName === "composerSelect") {
-
-} else if (initializedState.pageName === "composer") {
-    ReactDom.render(
-        <StyleRoot>
-            <ComposerPageComponent editToken={initializedState.editToken}/>
-        </StyleRoot>,
-        document.getElementById("app-container"));
-} else if (initializedState.pageName === "composer-view") {
-
 } else if (initializedState.pageName === "recorder") {
     ReactDom.render(
         <StyleRoot>
