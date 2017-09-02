2632e4c286a7698469716a3be4669ee327744b35
Ivan Chub <ivansergchub@gmail.com>
2017-09-01 18:27:21 -0700

    flootify page does the flootify request

--- src/components/pages/AutoComposePageComponent.tsx
+++ src/components/pages/AutoComposePageComponent.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
 import * as Radium from "radium";
 
+const getYoutubeId = require("get-youtube-id");
+const axios = require("axios");
+
 @Radium
 export class AutoComposePageComponent extends React.Component<IAutoComposePageComponentProps, IAutoComposePageComponentState> {
     props: IAutoComposePageComponentProps;
     state: IAutoComposePageComponentState;
 
+    refs: {
+        youtubeLink: HTMLInputElement
+    };
+
     constructor(props: IAutoComposePageComponentProps) {
         super();
+
+        this.state = {
+            stateName: AutoComposeStateName.Idle,
+        }
     }
 
     render() {
@@ -15,9 +26,48 @@ export class AutoComposePageComponent extends React.Component<IAutoComposePageCo
             <div style={[
                 AutoComposePageComponent.styles.base
             ]}>
-                AutoComposePageComponent
+                <label>
+                    youtube url:
+                    <input ref="youtubeLink" type="text"/>
+                </label>
+                <input type="submit" onClick={this.onSubmitClick.bind(this)}/>
+                <br/>
+                <br/>
+                state name: {this.state.stateName}
             </div>
         );
+    }
+
+    onSubmitClick() {
+        let videoId = getYoutubeId(this.refs.youtubeLink.value);
+
+        console.log(`new youtube url: ${videoId}`);
+
+        if (videoId !== null) {
+            this.flootify(videoId);
+        } else {
+
+        }
+    }
+
+    flootify(videoId: string) {
+        this.setState({
+            stateName: AutoComposeStateName.Flootifying,
+        });
+
+        axios.get(`/flootify/${videoId}`)
+            .then((result) => {
+                console.log("flootify complete");
+                this.setState({
+                    stateName: AutoComposeStateName.Flootified
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                this.setState({
+                    err: err as any
+                });
+            });
     }
 
     private static styles = {
@@ -27,6 +77,12 @@ export class AutoComposePageComponent extends React.Component<IAutoComposePageCo
     }
 }
 
+enum AutoComposeStateName {
+    Idle = "idle",
+    Flootifying = "flootifying",
+    Flootified = "flootified",
+}
+
 export interface IAutoComposePageComponentProps {
     isProd: boolean;
     editToken: string;
@@ -34,5 +90,5 @@ export interface IAutoComposePageComponentProps {
 }
 
 export interface IAutoComposePageComponentState {
-
+    stateName: AutoComposeStateName;
 }
