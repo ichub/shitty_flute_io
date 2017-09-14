aaa0ed9e81f9bb875db7812054dff154b473e0e5
briangu33 <briangu33@gmail.com>
2017-09-14 19:11:33 -0400

    D# unavailable note modal text

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -105,7 +105,7 @@ export const ModalStyle = {
         transitionDuration: "0.2s",
         transitionProperty: "all",
         transitionTimingFunction: "ease",
-        width: "450px",
+        width: "475px",
         "boxShadow": "0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)"
     }
 };
